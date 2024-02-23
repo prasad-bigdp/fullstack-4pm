b26e713c5c7c581d9df907e65bdaeaf82de91fa9
prasad-bigdp <bigdp585@gmail.com>
2024-02-23 17:39:00 +0530

    update

--- JS/Modals/script.js
+++ JS/Modals/script.js
@@ -3,18 +3,7 @@ function myOpen ()
 {
   
     const modal = document.getElementById("modal")
-    if (isOpen)
-    {
-        modal.style.display = "none";
-        isOpen=false
-        }
-       
-    else
-    {
-        modal.style.display = "block";
-        isOpen=true
-        document.body.style.background="rgba(0,0,0,0.4)"
-        }
+    modal.classList.toggle('d-none')
       
         
 }
