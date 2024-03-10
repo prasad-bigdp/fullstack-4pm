d8e383f8d7090b2eedba5dfe7a12a6de98f2a552
prasad-bigdp <bigdp585@gmail.com>
2024-03-10 21:13:28 +0530

    update

---
+++ JS/form-validation/form.js
@@ -0,0 +1,7 @@
+document.getElementById("signup").addEventListener('submit', signup)
+document.getElementById("loginform").addEventListener("submit", signIn)
+
+function signup()
+{
+    alert("signup clicked")
+}
