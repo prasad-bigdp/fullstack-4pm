9c185683f5c25b1f07869d5f8d5ea98304cace8f
prasad-bigdp <bigdp585@gmail.com>
2024-03-14 17:32:58 +0530

    update

--- mainProject/js/main.js
+++ mainProject/js/main.js
@@ -1,3 +1,59 @@
-const res = axios.get("https://dummyjson.com/products")
-const data = res.data;
-console.log(data)
+let products = [];
+function getData ()
+{
+    fetch("https://dummyjson.com/products")
+        .then((res) => res.json())
+        .then((data) =>
+        {
+            products = data.products;
+            filtering();
+            const swiper = new Swiper(".swiper", {
+							// Optional parameters
+							direction: "vertical",
+							loop: false,
+
+							// If we need pagination
+							pagination: {
+								el: ".swiper-pagination",
+							},
+
+							// Navigation arrows
+							navigation: {
+								nextEl: ".swiper-button-next",
+								prevEl: ".swiper-button-prev",
+							},
+
+							// And if we need scrollbar
+							scrollbar: {
+								el: ".swiper-scrollbar",
+							},
+						})
+
+      localStorage.setItem("products", JSON.stringify(products))
+    })
+}
+getData()
+
+function filtering ()
+{
+const Top_Products = products.filter((pro) => pro.rating > 4.8)
+    console.log(Top_Products)
+    Top_Products.forEach((p) =>
+    {
+        const imgDiv = document.createElement('div');
+        console.log(p)
+        const img = document.createElement('img');
+        img.src = p.thumbnail
+        img.alt = p.title
+        console.log(imgDiv,img)
+        imgDiv.appendChild(img)
+        const ele = document.querySelector('.main-slider')
+        ele.appendChild(imgDiv)
+                imgDiv.classList.add("swiper-slide")
+
+       console.log(ele)
+    })
+    
+}
+
+
